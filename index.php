b746af27b9e576b0864611fea35d77740a294656
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2021-08-14 23:30:59 +0200

    Feat Fetch the poll from the database
    Use $db connection to make pollsQuery
    to pull the questions with the defined
    date and stored them in an array

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php 
+require_once 'app/init.php';
+$pollsQuery=$db->query("
+    SELECT id, question
+    FROM polls
+    WHERE DATE(NOW()) BETWEEN starts AND ends");
+
+    while($row=$pollsQuery->fetchObject()){
+        $polls[]=$row;
+    }
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,9 +23,15 @@
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    <ul>
-        <li><a href="poll.php">Poll 1</a></li>
-        <li><a href="poll.php">Poll 1</a></li>
-    </ul>
+    <?php if(!empty($polls)): ?>
+        <ul>
+            <?php foreach($polls as $poll): ?>
+            <li><a href="poll.php?id=<?php echo $poll->id?>"><?php echo $poll->question;?></a></li>
+           <?php endforeach;?>
+      </ul>
+    <?php else: ?>
+        <p> Sorry, no polls availabe right now.</p>
+    <?php endif; ?>
+    
 </body>
 </html>
